17fff53a0b15eca97f7972f7a65b786544e5ca1e
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2024-02-11 23:29:06 +0530

    Add custom create method to the StudentanswerSerializers

--- ExamResponse/serializers.py
+++ ExamResponse/serializers.py
@@ -1,19 +1,22 @@
 from rest_framework import serializers
-from .models import Studentanswer, Student_answer
+
+from .models import Student_answer, Studentanswer
 
 
 class StudentAnswerSerializers(serializers.ModelSerializer):
     class Meta:
         model = Student_answer
         fields = ("id", "question_number", "answer_text")
-        depth=2
+        depth = 2
+
     # def get_student_answers(self, obj):
     #     student_answers = Student_answer.objects.filter(student_exam=obj)
     #     return StudentAnswerAnswerSerializers(student_answers, many=True).data
-        
+
+
 # class StudentAnswerAnswerSerializers(serializers.ModelSerializer):
-#     # student_exam = StudentAnswerSerializers(many=True) 
-#     student_answers = serializers.SerializerMethodField() 
+#     # student_exam = StudentAnswerSerializers(many=True)
+#     student_answers = serializers.SerializerMethodField()
 
 #     class Meta:
 #         model = Studentanswer
@@ -22,13 +25,13 @@ class StudentAnswerSerializers(serializers.ModelSerializer):
 #         depth = 2
 
 #     # def create(self, validated_data):
-#     #     answers_data = validated_data.pop("student_exam")  
+#     #     answers_data = validated_data.pop("student_exam")
 #     #     data = super().create(validated_data)
 #     #     if answers_data is not None:
 #     #         for answer_data in answers_data:
 #     #             Student_answer.objects.create(student_answers=data, **answer_data)
 #     #     return data
-        
+
 #     def get_student_answers(self, obj):
 #         student_answers = Student_answer.objects.filter(student_exam=obj)
 #         serialized_answers = StudentAnswerAnswerSerializers(student_answers, many=True).data
@@ -42,9 +45,10 @@ class StudentanswerSerializers(serializers.ModelSerializer):
         model = Studentanswer
         fields = ("id", "user", "exam", "student_exam")
 
-    # def create(self, validated_data):
-    #     student_exam_data = validated_data.pop("student_exam")
-    #     studentanswer = Studentanswer.objects.create(**validated_data)
-    #     for answer_data in student_exam_data:
-    #         Student_answer.objects.create(student_answers=studentanswer, **answer_data)
-        # return studentanswer
+    def create(self, validated_data):
+        student_exam_data = validated_data.pop("student_exam")
+        studentanswer = Studentanswer.objects.create(**validated_data)
+        for answer_data in student_exam_data:
+            Student_answer.objects.create(student_answers=studentanswer, **answer_data)
+
+        return studentanswer
