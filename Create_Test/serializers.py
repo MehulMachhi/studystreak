74add259ba7fa89fe1daad0d26d4bbc57e29ceaf
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2024-02-18 12:22:17 +0530

    Add exam_type in createexamserializers

--- Create_Test/serializers.py
+++ Create_Test/serializers.py
@@ -1,14 +1,35 @@
+from functools import reduce
+
 from rest_framework import serializers
 
 from .models import Responses, createexam, module
 
 
 class createexamserializers(serializers.ModelSerializer):
+    exam_type  = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = createexam
         fields = "__all__"
         depth = 2
 
+    def get_exam_type(self, obj):
+        
+        listening= obj.IELTS.Listening.exists()
+        speaking= obj.IELTS.Speaking.exists()
+        writing= obj.IELTS.Writing.exists()
+        
+        count = reduce(lambda x, y: bool(x) + bool(y), [listening, speaking, writing])
+        if count >= 2:
+            raise serializers.ValidationError("Backend has more than one exam_type, please remove one of them.")
+        
+        if listening:
+            exam_type = 'Listening'
+        elif speaking:
+            exam_type = 'Speaking'
+        elif writing:
+            exam_type = 'Writing'
+            
+        return exam_type
 
 class ResponsesSerializers(serializers.ModelSerializer):
     class Meta:
