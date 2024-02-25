c8c3474f83c540b7905ca64cd27cdb19c9e960aa
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2024-02-25 23:57:15 +0530

    minor changes

--- LiveClass/models.py
+++ LiveClass/models.py
@@ -14,7 +14,7 @@ class Live_Class(models.Model):
     end_time = models.DateTimeField(null=True, blank=True)
     zoom_meeting_id = models.CharField(max_length=100, blank=True, null=False)
     zoom_meeting_password = models.CharField(max_length=100, blank=True, null=False)
-    registration_limit = models.PositiveIntegerField()
+    registration_limit = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.meeting_title}"
