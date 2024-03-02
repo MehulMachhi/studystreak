b4b923c2b733a0f4a2ba46aa9510e11df46e86bb
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2024-03-02 16:41:22 +0530

    Add Full_Length_Exam and exam_type in  Studentanswer

--- ExamResponse/models.py
+++ ExamResponse/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from Create_Test.models import FullLengthTest, module
 # Create your models here.
 # Test
-from exam.models import Exam
-from Create_Test.models import module
+from exam.models import Exam, ExamType
+
 
 class Studentanswer(models.Model):
     user = models.ForeignKey(
@@ -13,10 +14,11 @@ class Studentanswer(models.Model):
     )
     exam = models.ForeignKey(Exam, related_name="exam", on_delete=models.CASCADE, null=True, blank=True)
     Practise_Exam = models.ForeignKey(module, on_delete=models.CASCADE, null=True, blank=True)
-    Full_Length_Exam = models.ForeignKey(module, on_delete=models.CASCADE, null=True, blank=True, related_name = "gull_length+")
+    Full_Length_Exam = models.ForeignKey(FullLengthTest, on_delete=models.CASCADE, null=True, blank=True, related_name = "full_length+")
     AI_Assessment = models.TextField(null=True, blank=True)
     Tutor_Assessment = models.TextField(null=True, blank=True)
     band = models.CharField(null=True, blank=True)
+    exam_type = models.CharField(max_length=30, choices = ExamType.choices, null=True, blank=True)
 
 class Student_answer(models.Model):
     student_answers = models.ForeignKey(
