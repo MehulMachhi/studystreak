ee7c2804f40d93c08c749007893524724aeb03f9
MehulMachhi <machhimehul61@gmail.com>
2024-03-04 12:13:51 +0530

    field change in product Order Model

--- payment/models.py
+++ payment/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from package.models import Package
 # Create your models here.
 
 
 class Order(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    product = models.CharField(max_length=100)
+    product = models.ManyToManyField(Package)
     amount = models.IntegerField()
     payment_id = models.CharField(max_length=100)
     signature_id = models.CharField(max_length=100)
