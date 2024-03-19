c8060014260103ffecf0721ef8c3a026ff69b845
asc <asc@jeegar@ASC.COM>
2024-03-19 22:38:08 +0530

    Add the filter logic for liveClass Django-Admin

--- LiveClass/admin.py
+++ LiveClass/admin.py
@@ -1,17 +1,34 @@
 from datetime import date, datetime, time
+from typing import Any
 
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 
 from .models import Live_Class
-
+from django.utils.html import format_html
+from Courses.models import Course
+from django.db.models import Q
 
 class LiveClassAdmin(admin.ModelAdmin):
     list_display = ['meeting_title', 'start_time', 'end_time', "select_batch", "liveclasstype","meeting_title", "start_time",
-                    "end_time", "zoom_meeting_id"]
+                    "end_time", "meeting_url"]
     search_fields = ['meeting_title']
     ordering = ['start_time']
     list_filter = ("select_batch", "liveclasstype", "meeting_title", "zoom_meeting_id", "bookslot_count", )
     search_fields = ("select_batch", "liveclasstype", "meeting_title", "zoom_meeting_id", "bookslot_count", )
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(
+                Q(select_batch__add_package__select_course__tutor = request.user) |
+                Q(select_batch__add_package__select_course__primary_instructor = request.user))
+
+    def meeting_url(self, obj):
+        return format_html("<a href='{url}'>{url}</a>", url=obj.zoom_meeting_id)
 
 admin.site.register(Live_Class, LiveClassAdmin)
 
