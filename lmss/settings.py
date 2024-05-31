32a522daeee51fe370f4c87e769d5473547e3e8e
amitpatel <amit.patel@vayana.com>
2024-05-31 10:54:53 +0530

    setting accessToken time to 1 min

--- lmss/settings.py
+++ lmss/settings.py
@@ -281,7 +281,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
 }
 
